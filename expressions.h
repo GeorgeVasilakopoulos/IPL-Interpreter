329c7aae9761467c1d602cbff973709a4cf00275
GeorgeVasilakopoulos <96617492+GeorgeVasilakopoulos@users.noreply.github.com>
2022-09-16 01:17:02 +0300

    Delete expressions.h

--- expressions.h
+++
@@ -1,6 +0,0 @@
-char* getVariableFromString(char* name_output, int* index_output, char* string);
-char* getNumberFromString(int* value_output, char* string, char* end_of_num);
-int evaluateExpression(char* expression);
-
-
-
